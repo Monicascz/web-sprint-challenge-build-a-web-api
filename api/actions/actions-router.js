7f34b97056537b5f310b31ba3bd3ef493241a611
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-04-09 09:27:01 -0700

    get by id is working with middleware added

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,10 +1,11 @@
 // Write your "actions" router here!
 const express = require("express")
 
+
 const router = express.Router();
 
 const Actions = require('./actions-model.js')
-
+const mw = require('../middlewares/middlewares.js')
 
 //- `[GET] /` returns an array of actions (or an empty array) as the body of the _response_.
 router.get('/', (req,res)=>{
@@ -19,11 +20,18 @@ router.get('/', (req,res)=>{
 
 
 //- `[GET] `/:id` returns an action with the given `id` as the body of the _response_.
-// router.get('/:id', (req,res)=>{
-//     const {id}= req.params
-//     Actions.get(id)
-//     .then()
-// })
+router.get('/:id', mw.checkActionId, (req,res)=>{
+    const {id}= req.params
+    Actions.get(id)
+    .then(action=>{
+        res.status(200).json(action)
+    })
+    .catch(()=>{
+        res.status(500).json({message: "Error getting the actions."})
+    })
+})
+
+
 //- `[POST] `/` returns the newly created action as the body of the _response_.
 // router.post('/', (req,res)=>{})
 // //- `[PUT] `/:id` returns the updated action as the body of the _response_.
---
+++ api/middlewares/middlewares.js
@@ -0,0 +1,15 @@
+const Actions = require('../actions/actions-model.js')
+
+const checkActionId = async (req,res,next)=>{
+    const {id}=req.params
+    const action = await Actions.get(id)
+    if(!action){
+        res.status(404).json({ message: "action not found" })
+    }else{
+        next()
+    }
+}
+
+module.exports={
+    checkActionId
+}
