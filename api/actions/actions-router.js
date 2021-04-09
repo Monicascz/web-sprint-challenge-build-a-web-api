9c3f060bdaaaff6de16bde470ab8d0552ca25814
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-04-09 09:19:42 -0700

    actionsRouter connected and getting the array of actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,34 @@
 // Write your "actions" router here!
+const express = require("express")
+
+const router = express.Router();
+
+const Actions = require('./actions-model.js')
+
+
+//- `[GET] /` returns an array of actions (or an empty array) as the body of the _response_.
+router.get('/', (req,res)=>{
+    Actions.get()
+    .then(action=>{
+        res.status(200).json(action)
+    })
+    .catch(()=>{
+        res.status(500).json({message: "Error getting the actions."})
+    })
+})
+
+
+//- `[GET] `/:id` returns an action with the given `id` as the body of the _response_.
+// router.get('/:id', (req,res)=>{
+//     const {id}= req.params
+//     Actions.get(id)
+//     .then()
+// })
+//- `[POST] `/` returns the newly created action as the body of the _response_.
+// router.post('/', (req,res)=>{})
+// //- `[PUT] `/:id` returns the updated action as the body of the _response_.
+// router.put('/:id', (req,res)=>{})
+// //- `[DELETE] `/:id` returns no _response_ body.
+// router.delete('/:id', (req,res)=>{})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const server = express();
+const actionRouter = require('./actions/actions-router.js')
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
 server.use(express.json())
+server.use('/api/actions', actionRouter)
 
 server.get('/', (req,res)=>{
     res.send(`<h1> Let's pass this sprint! </h1>`)
